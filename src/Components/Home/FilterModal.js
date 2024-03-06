5218d1739f4e8277b2450ceea0dd1b8300f92ad3
henok26 <henokaniguse1931@gmail.com>
2024-03-06 17:27:02 +0300

    FILTER-modal component refined

--- src/Components/Home/FilterModal.js
+++ src/Components/Home/FilterModal.js
@@ -1,37 +1,63 @@
-import React ,{useEffect,useState}from 'react'
-import PropTypes from 'prop-types' //for prop types checking
-import '../../CSS/FilterModal.css';
-import 'react-input-range/lib/css/index.css'
-import InputRange from 'react-input-range'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types"; //for prop types checking
+import "../../CSS/FilterModal.css";
+import "react-input-range/lib/css/index.css";
+import InputRange from "react-input-range";
 
-const FilterModal = (selectedFilters,onFilterChange,onClose) => {
-    const [priceRange,setPriceRange]=useState({min:selectedFilters.priceRange?.min || 600,
-    max:selectedFilters.priceRange?.max || 30000,
-    })
-    const [propertyType,setPropertyType]=useState(selectedFilters.propertyType||'') // default property
+const FilterModal = (selectedFilters, onFilterChange, onClose) => {
+  const [priceRange, setPriceRange] = useState({
+    min: selectedFilters.priceRange?.min || 600,
+    max: selectedFilters.priceRange?.max || 30000,
+  });
+  const [propertyType, setPropertyType] = useState(
+    selectedFilters.propertyType || ""
+  ); // default property
 
-    const[roomType,setRoomType]=useState(selectedFilters.roomType || '')
+  const [roomType, setRoomType] = useState(selectedFilters.roomType || "");
 
-    const[amenities,setAmenities]=useState(selectedFilters.amenities || [])
+  const [amenities, setAmenities] = useState(selectedFilters.amenities || []);
 
-    //use effect hooks  to update states when selected filters prop changes
-    useEffect(()=>{
+  //use effect hooks  to update states when selected filters prop changes
+  useEffect(() => {
+    setPriceRange({
+      min: selectedFilters.priceRange?.min || 600,
+      max: selectedFilters.priceRange?.max || 30000,
+    });
 
-        setPriceRange({min:selectedFilters.priceRange?.min || 600,
-            max:selectedFilters.priceRange?.max || 30000,}
+    setPropertyType(selectedFilters.propertyType || "");
+    setRoomType(selectedFilters.roomType || "");
+    setAmenities(selectedFilters.amenities || []);
+   }, [selectedFilters]);
+  
 
+  //function to handle changes in price range
+  const handlePriceRangeChange = (value) => {
+    setPriceRange(value); //it will update the price range state
+  };
+  //function to handle min value
+  const handleMinInputChange = (e) => {
+    const minValue = parseInt(e.target.value, 10);
+    setPriceRange((prev) => ({ ...prev, min: minValue }));
+  };
 
+  //function to handle max value
+  const handleMaxInputChange = (e) => {
+    const maxValue = parseInt(e.target.value, 10);
+    setPriceRange((prev) => ({ ...prev, max: maxValue }));
+  };
+  //function to handle applying filters
+  const handleFilterChange=()=>{
 
-        );
+onFilterChange('minPrice',priceRange.min);
+onFilterChange('maxPrice',priceRange.max);
+onFilterChange('propertyType',propertyType);
+onFilterChange('roomType',roomType);
+onFilterChange('amenities',amenities);
+onclose();
 
-    
-    setPropertyType(selectedFilters.propertyType || '');
-    setRoomType(selectedFilters.roomType || '');
-    setAmenities(selectedFilters.amenities || [])}
-    ) ;
-  return (
-    <div>FilterModal</div>
-  )
-}
+  }
 
-export default FilterModal
+  return <div>FilterModal</div>;
+};
+
+export default FilterModal;
