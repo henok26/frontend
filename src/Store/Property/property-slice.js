5b9bbbba0cdda2e9516bf27bad24f21d12c45fa1
henok26 <henokaniguse1931@gmail.com>
2024-03-16 17:34:54 +0300

    property-action file addes

--- src/Store/Property/property-slice.js
+++ src/Store/Property/property-slice.js
@@ -1,28 +1,42 @@
-import {createSlice} from '@reduxjs/toolkit'
-const propertySlice=createSlice({
-//slice name
-name:"property",
-//initial state for the property slice
-initialState:{
-    properties:[],
-    totalProperties:0,
-    searchParams:{},//parameters used to search
-    error:null,//error state
-    loading:false,//loading sate for the property
-},
-reducers:{
-    getRequest(state){
-        state.loading=true;
+import { createSlice } from "@reduxjs/toolkit";
+const propertySlice = createSlice({
+  //slice name
+  name: "property",
+  //initial state for the property slice
+  initialState: {
+    properties: [],
+    totalProperties: 0,
+    searchParams: {}, //parameters used to search
+    error: null, //error state
+    loading: false, //loading sate for the property
+  },
+  reducers: {
+    getRequest(state) {
+      state.loading = true;
     },
-    getProperties(state,action){
-        state.properties=action.payload.data;
-        state.totalProperties=action.payload.all_properties;
-        state.totalProperties=action.payload.all_properties;
-        state.loading=false;
-
+    getProperties(state, action) {
+      state.properties = action.payload.data;
+      state.totalProperties = action.payload.all_properties;
+      state.totalProperties = action.payload.all_properties;
+      state.loading = false;
     },
-    //
-},
 
-})
+    //Action to search parameters
+    updateSearchParams: (state, action) => {
+      state.searchParams =
+        object.keys(action.payload).length === 0
+          ? {}
+          : {
+              ...state.searchParams,
+              ...action.payload,
+            };
+    },
+    //action to update error state
+    getErrors(state,action){
 
+        state.error=action.payload;
+    },
+  },
+});
+export const propertyAction=propertySlice.actions;
+export default  propertySlice;
