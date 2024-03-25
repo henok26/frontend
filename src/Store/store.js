bbcee79157f0f0f20b262dbded47560788d1366d
henok26 <henokaniguse1931@gmail.com>
2024-03-25 17:16:00 +0300

    property details reducer added to store.js file

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,11 +2,13 @@
 //creates empty container
 import {configureStore} from "@reduxjs/toolkit"
 import propertySlice from "./Property/property-slice";
+import propertyDetailsSlice from "./PropertyDetails/propertyDetails-slice";
 const store=configureStore({
 
     reducer:{
 
 properties:propertySlice.reducer,
+propertydetails:propertyDetailsSlice.reducer,
 
     },
 });
