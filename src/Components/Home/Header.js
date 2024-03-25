03e2e999ba6715d4b96957a1c0650cf7a08eb31a
henok26 <henokaniguse1931@gmail.com>
2024-03-25 17:09:42 +0300

    property-deatail-action completed

--- src/Components/Home/Header.js
+++ src/Components/Home/Header.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Search from './Search';
 import Filter from './Filter';
+import {Link} from 'react-router-dom';
+import { UseDispatch, useDispatch } from 'react-redux';
+import { propertyAction } from '../../Store/Property/property-slice';
+import { getAllProperties } from '../../Store/Property/property-action';
 const Header = () => {
+  const dispatch=useDispatch();
+  const allproperties=()=>{
+    
+    dispatch(propertyAction.updateSearchParams({}));
+    dispatch(getAllProperties());
+
+
+  }
   return (
     <>
       <nav className='header row sticky-top'>
-<img src="/assets/logo.png" alt="logo"className="logo"/> 
-      
+
+      <Link to="/">
+      <img src="/assets/logo.png" alt="logo"className="logo" onClick={allproperties}/> 
+      </Link>
       <div className='search_filter'>
       <Search/>
       <Filter/>
---
+++ src/Store/PropertyDetails/propertyDetails-slice.js
@@ -0,0 +1,33 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+
+const propertyDetailsSlice=createSlice({
+
+name:"propertyDetails",
+initialState:{
+    propertydetails:[],
+    loading:false,
+    error:null,
+},
+reducers:{
+getListRequest(state){
+state.loading=true;
+
+},
+//update property details in the state successfully fetching them
+getPropertyDetails(state,action){
+    state.propertydetails=action.payload;
+    state.loading=false;
+},
+getErrors(state,action){
+
+    state.error=action.payload;
+    state.loading=false;
+},
+
+},
+
+
+})
+export const propertyDetailsAction=propertyDetailsSlice.actions;
+export default propertyDetailsSlice;
