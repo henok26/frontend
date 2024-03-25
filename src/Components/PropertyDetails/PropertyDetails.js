31058c59693473d7d360c721fd77bae090f8f594
henok26 <henokaniguse1931@gmail.com>
2024-03-25 17:29:49 +0300

    Property details component started

---
+++ src/Components/PropertyDetails/PropertyDetails.js
@@ -0,0 +1,12 @@
+import React ,{useEffect} from 'react';
+import"../../CSS/PropertyDetails.css";
+import {useDispatch,useSelector} from 'react-redux'
+
+
+const PropertyDetails = () => {
+  return (
+    <div>PropertyDetails</div>
+  )
+}
+
+export default PropertyDetails
