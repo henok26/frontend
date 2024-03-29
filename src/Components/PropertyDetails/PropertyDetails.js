c1bc2ba2a06625a177a4fe01eeeacb3179039fa1
henok26 <henokaniguse1931@gmail.com>
2024-03-29 12:17:40 +0300

    booking form component created

--- src/Components/PropertyDetails/PropertyDetails.js
+++ src/Components/PropertyDetails/PropertyDetails.js
@@ -1,58 +1,60 @@
-import React ,{useEffect} from 'react';
-import"../../CSS/PropertyDetails.css";
-import {useDispatch,useSelector} from 'react-redux'
-import {useParams} from 'react-router-dom';
-import { getPropertyDetails } from '../../Store/PropertyDetails/propertyDetails-action';
-import PropertyImg from './PropertyImg';
-import PropertyAmenities from '../../Store/PropertyDetails/PropertyAmenities';
+import React, { useEffect } from "react";
+import "../../CSS/PropertyDetails.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getPropertyDetails } from "../../Store/PropertyDetails/propertyDetails-action";
+import PropertyImg from "./PropertyImg";
+import PropertyAmenities from "../../Store/PropertyDetails/PropertyAmenities";
+import BookingForm from "./BookingForm";
 
 const PropertyDetails = () => {
-  const dispatch=useDispatch();
-  const {id}=useParams();
-  const {propertydetails}=useSelector((state)=>state.propertydetails);
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const { propertydetails } = useSelector((state) => state.propertydetails);
   console.log(propertydetails);
-  useEffect(()=>{
-dispatch(getPropertyDetails(id));
-
-
-  },[dispatch,id]);
-  const {propertyName,address,images,description,maximumGuest,amenities}=propertydetails;
+  useEffect(() => {
+    dispatch(getPropertyDetails(id));
+  }, [dispatch, id]);
+  const {
+    propertyName,
+    address,
+    images,
+    description,
+    maximumGuest,
+    amenities,
+    currentBookings
+  } = propertydetails;
   return (
-   <div className='property-container'>
-{
+    <div className="property-container">
+      {propertyName && (
+        <>
+          <p className="property-header">{propertyName} </p>
+          <h6 className="property-location">
+            <span class="material-symbols-outlined">house</span>
+            <span className="location">
+              {`${address.area},${address.city},${address.pincode},${address.state}`}{" "}
+            </span>
+          </h6>
+          <PropertyImg images={images} />
+          <div className="middle-container row ">
+            <div className="des-and-amenities col-md-8 col-sm-12 col-12">
+              <h2 className="property-description-header">Description</h2>
+              <p className="property-description">
+                {description} <br></br>
+                <br />
+                Max number of Guest:{maximumGuest} <br />
+                <br />
+              </p>
+              <hr />
+              <PropertyAmenities amenities={amenities} />
+              <br /> <br />
+              <BookingForm/>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
-  propertyName && (<>
-  <p className='property-header'>
-   {propertyName}   </p>
-   <h6 className='property-location'>
-
-   <span class="material-symbols-outlined">
-house
-</span>
-<span className="location">{`${address.area},${address.city},${address.pincode},${address.state}`}  </span>
-
-   </h6>
-   <PropertyImg images={images}/>
-   <div className='middle-container row '>
-<div className='des-and-amenities col-md-8 col-sm-12 col-12'>
-  <h2 className='property-description-header'>
-Description</h2>
-<p className='property-description'>
-{description} <br></br><br/>
-Max number of Guest:{maximumGuest} <br/><br/>
-</p>
-<hr/>
-<PropertyAmenities amenities ={amenities}/>  
-<br/> <br/>
-
-</div>
-   </div>
-
-  </> )
-}
-   </div>
-
-  )
-}
-
-export default PropertyDetails
+export default PropertyDetails;
